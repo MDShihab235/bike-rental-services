480465786b7176aa5daa03c7d952b9f233ebdab7
MikatSyed <syednew5000@gmail.com>
2022-09-03 19:04:45 +0600

    bike rental service

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,7 +4,7 @@ const sendEmail = async (options) => {
   const transporter = nodeMailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    server: process.env.SMTP_product,
+    server: process.env.SMTP_PRODUCT,
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
